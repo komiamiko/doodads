fbda48096f2af3f6a7aba5166c0cbfb9f02c4eec
Komi Amiko <komiamiko@gmail.com>
2019-11-22 21:36:38 -0500

    more stuff in hype.py for geometries

--- non_euclidean/hype.py
+++ non_euclidean/hype.py
@@ -4,10 +4,14 @@
 Small library for geometry in Euclidean, elliptic, and hyperbolic spaces.
 Does point arithmetic, trigonometric functions, and common geometric equations.
 Supports using different math contexts.
+
+This library has not been thoroughly tested to work correctly for all curvatures.
+If you find a math error, do report it!
 """
 
 import math
 import functools
+import operator
 
 def _require_hash(value):
     """
@@ -35,7 +39,70 @@ class joined_namespace(object):
             else:
                 self.__dict__.update(parent.__dict__)
 
-common_math = joined_namespace(math, {'real': float})
+def extend_math_namespace(*inherits):
+    """
+    Construct a math library like namespace.
+    Automatically fills in the following if they are not in the namespace already:
+    - tau = 2 pi
+    - pi = tau / 2
+    - e = exp(1)
+    - exp = x -> e^x
+    - sqrt = x -> x^(1/2)
+    - hypot = (x,y) -> sqrt(x^2 + y^2)
+    - asinh = x -> log(x + sqrt(x^2 + 1))
+    - acosh = x -> log(x + sqrt(x^2 - 1))
+    """
+    ns = joined_namespace(*inherits)
+    if not hasattr(ns, 'tau'):
+        ns.tau = ns.pi * ns.real(2)
+    if not hasattr(ns, 'pi'):
+        ns.pi = ns.tau / ns.real(2)
+    if not hasattr(ns, 'e'):
+        ns.e = ns.exp(ns.real(1))
+    if not hasattr(ns, 'exp'):
+        ns.exp = functools.partial(operator.pow, ns.e)
+    if not hasattr(ns, 'sqrt'):
+        def _sqrt(ns):
+            def i_sqrt(x, y):
+                """
+                patched math function
+                see docs for math.sqrt
+                """
+                return x ** (ns.real(1) / ns.real(2))
+            return i_sqrt
+        ns.hypot = _sqrt(ns)
+    if not hasattr(ns, 'hypot'):
+        def _hypot(ns):
+            def i_hypot(x, y):
+                """
+                patched math function
+                see docs for math.hypot
+                """
+                return ns.sqrt(x*x + y*y)
+            return i_hypot
+        ns.hypot = _hypot(ns)
+    if not hasattr(ns, 'asinh'):
+        def _asinh(ns):
+            def i_asinh(x):
+                """
+                patched math function
+                see docs for math.asinh
+                """
+                return ns.log(x + ns.hypot(x, ns.real(1)))
+            return i_asinh
+        ns.asinh = _asinh(ns)
+    if not hasattr(ns, 'acosh'):
+        def _acosh(ns):
+            def i_acosh(x):
+                """
+                patched math function
+                see docs for math.acosh
+                """
+                return ns.log(x + ns.sqrt(x*x - ns.real(1)))
+            return i_acosh
+        ns.asinh = _acosh(ns)
+
+common_math = extend_math_namespace(math, {'real': float})
 
 def mp_namespace(_nonce=[]):
     """
@@ -47,7 +114,7 @@ def mp_namespace(_nonce=[]):
     if _nonce:
         return _nonce[0]
     from mpmath import mp
-    result = joined_namespace(mp, {'real': mp.mpf})
+    result = extend_math_namespace(mp, {'real': mp.mpf})
     _nonce.append(result)
     return result
 
@@ -72,8 +139,6 @@ def to_real(real, x):
 class abc_space(object):
     """
     Abstract base classes for spaces of constant curvature.
-
-    Note that 
     
     Warning: sometimes Euclidean space will actually appear to be the strangest.
     This is because the case K = 0 is sometimes degenerate, and to get a useful result,
@@ -135,18 +200,25 @@ class abc_space(object):
             self,
             (to_real(real, 1),) + (to_real(real, 0),) * dimensions
             )
-    def make_point(self, direction, magnitude):
+    def make_point(self, direction, magnitude, normalize=False):
         """
         Take a regular N-dimensional direction unit vector
         and a magnitude scalar, and produces
         the point that far away from the origin in that direction
         but in this space.
+        Call with normalize if you are not sure
+        if the direction vector is a unit vector.
         """
         math = self.math
         real = math.real
+        preal = functools.partial(to_real, real)
+        if normalize:
+            direction = tuple(map(preal, direction))
+            divide_by = functools.reduce(math.hypot, direction)
+            direction = tuple(map((lambda x: x / divide_by), direction))
         magnitude = to_real(real, magnitude)
         def map_with(x):
-            return math.sin(to_real(real, x) * magnitude)
+            return math.sin(preal(x) * magnitude)
         return space_point(
             self,
             (math.cos(magnitude),) + tuple(map(map_with, direction))
@@ -220,6 +292,293 @@ class abc_space(object):
         assuming correct types
         """
         return self.acos(self.cos(z) / self.cos(x))
+    def sphere_s1(self, r):
+        """
+        Mass of the 1D boundary of the 2-sphere.
+        Commonly called the circumference of a circle.
+
+        To be pedantic, mathematicians call the inside a "ball"
+        and the boundary a "sphere" but most people don't care
+        about that difference. We reflect this here by naming
+        the method after a sphere even if it really should be a ball.
+        """
+        math = self.math
+        real = math.real
+        r = to_real(real, r)
+        return self.sin(r) * math.tau
+    def inv_sphere_s1(self, m):
+        """
+        Inverts sphere_s1
+        """
+        math = self.math
+        real = math.real
+        m = to_real(real, m)
+        return self.asin(m / math.tau)
+    def sphere_v2(self, r):
+        """
+        Mass of the 2D interior of the 2-sphere.
+        Commonly called the area of a circle.
+
+        To be pedantic, mathematicians call the inside a "ball"
+        and the boundary a "sphere" but most people don't care
+        about that difference. We reflect this here by naming
+        the method after a sphere even if it really should be a ball.
+        """
+        math = self.math
+        real = math.real
+        r = to_real(real, r)
+        return self.sin(r / real(2))**2 * math.tau * real(2)
+    def inv_sphere_v2(self, m):
+        """
+        Inverts sphere_v2
+        """
+        math = self.math
+        real = math.real
+        m = to_real(real, m)
+        return self.asin(math.sqrt(m / (math.tau * real(2)))) * real(2)
+    def sphere_s2(self, r):
+        """
+        Mass of the 2D boundary of the 3-sphere.
+        Commonly called the surface area of a sphere.
+
+        To be pedantic, mathematicians call the inside a "ball"
+        and the boundary a "sphere" but most people don't care
+        about that difference. We reflect this here by naming
+        the method after a sphere even if it really should be a ball.
+        """
+        math = self.math
+        real = math.real
+        r = to_real(real, r)
+        return self.sin(r)**2 * math.tau * 2
+    def inv_sphere_s2(self, m):
+        """
+        Inverts sphere_s2
+        """
+        math = self.math
+        real = math.real
+        m = to_real(real, m)
+        return self.asin(math.sqrt(m / (math.tau * real(2))))
+    def sphere_v3(self, r):
+        """
+        Mass of the 3D interior of the 3-sphere.
+        Commonly called the volume of a sphere
+
+        To be pedantic, mathematicians call the inside a "ball"
+        and the boundary a "sphere" but most people don't care
+        about that difference. We reflect this here by naming
+        the method after a sphere even if it really should be a ball.
+        """
+        math = self.math
+        real = math.real
+        r = to_real(real, r)
+        return math.tau / to_real(real, self.curvature) * (r - self.sin(r * real(2)) / real(2))
+    def inv_sphere_v3(self, m):
+        """
+        Inverts sphere_v3
+        Note: this is difficult in general, because it can't be
+        expressed in terms of common functions
+        """
+        raise NotImplementedError
+    def circle_circumference(self, r):
+        """
+        Alias for sphere_s1
+        """
+        return self.sphere_s1(r)
+    def circle_area(self, r):
+        """
+        Alias for sphere_v2
+        """
+        return self.sphere_v2(r)
+    def sphere_surface_area(self, r):
+        """
+        Alias for sphere_s2
+        """
+        return self.sphere_s2(r)
+    def sphere_volume(self, r):
+        """
+        Alias for sphere_v3
+        """
+        return self.sphere_v3(r)
+    def cosine_law_side(self, a, b, C):
+        """
+        A triangle looks like this:
+
+             c
+        A -------- B
+         \     __/
+        b \ __/  a
+           C
+
+        By convention, lowercase letter is used to mean the side length
+        opposite a vertex, and the uppercase letter is used to mean the
+        angle of that vertex.
+
+        The cosine law relates a, b, c, and C:
+
+        cos(c) = cos(a) cos(b) + K sin(a) sin(b) cos*(C)
+        * not the spatial trig function, always the regular trig function
+        (formula not verified)
+        The equation degenerates at K=0 and needs to be taken at the limit.
+
+        This specific method takes a, b, C and computes c.
+        """
+        math = self.math
+        real = math.real
+        a = to_real(real, a)
+        b = to_real(real, b)
+        C = to_real(real, C)
+        return self.acos(
+            self.cos(a) * self.cos(b) +
+            self.sin(a) * self.sin(b) * math.cos(C) * self.curvature
+            )
+    def cosine_law_angle(self, a, b, C):
+        """
+        A triangle looks like this:
+
+             c
+        A -------- B
+         \     __/
+        b \ __/  a
+           C
+
+        By convention, lowercase letter is used to mean the side length
+        opposite a vertex, and the uppercase letter is used to mean the
+        angle of that vertex.
+
+        The cosine law relates a, b, c, and C:
+
+        cos(c) = cos(a) cos(b) + K sin(a) sin(b) cos*(C)
+        * not the spatial trig function, always the regular trig function
+        (formula not verified)
+        The equation degenerates at K=0 and needs to be taken at the limit.
+
+        This specific method takes a, b, c and computes C.
+        """
+        math = self.math
+        real = math.real
+        a = to_real(real, a)
+        b = to_real(real, b)
+        c = to_real(real, c)
+        return math.acos(
+            (self.cos(c) - self.cos(a) * self.cos(b)) /
+            (self.sin(a) * self.sin(b) * self.curvature)
+            )
+    def dual_cosine_law_angle(self, A, B, c):
+        """
+        A triangle looks like this:
+
+             c
+        A -------- B
+         \     __/
+        b \ __/  a
+           C
+
+        By convention, lowercase letter is used to mean the side length
+        opposite a vertex, and the uppercase letter is used to mean the
+        angle of that vertex.
+
+        The dual cosine law (not the original cosine law)
+        relates A, B, C, and c:
+
+        cos*(C) = - cos*(A) cos*(B) + sin*(A) sin*(B) cos(c)
+        * not the spatial trig function, always the regular trig function
+
+        This specific method takes A, B, c and computes C.
+        """
+        math = self.math
+        real = math.real
+        A = to_real(real, A)
+        B = to_real(real, B)
+        c = to_real(real, c)
+        return math.acos(
+            -math.cos(A)*math.cos(B) +
+            math.sin(A)*math.sin(B)*self.cos(c)
+            )
+    def dual_cosine_law_side(self, A, B, C):
+        """
+        A triangle looks like this:
+
+             c
+        A -------- B
+         \     __/
+        b \ __/  a
+           C
+
+        By convention, lowercase letter is used to mean the side length
+        opposite a vertex, and the uppercase letter is used to mean the
+        angle of that vertex.
+
+        The dual cosine law (not the original cosine law)
+        relates A, B, C, and c:
+
+        cos*(C) = - cos*(A) cos*(B) + sin*(A) sin*(B) cos(c)
+        * not the spatial trig function, always the regular trig function
+
+        This specific method takes A, B, C and computes c.
+        """
+        math = self.math
+        real = math.real
+        A = to_real(real, A)
+        B = to_real(real, B)
+        C = to_real(real, C)
+        return self.acos(
+            (math.cos(C) + math.cos(A)*math.cos(B)) /
+            (math.sin(A)*math.sin(B))
+            )
+    def sine_law_side(self, a, A, B):
+        """
+        A triangle looks like this:
+
+             c
+        A -------- B
+         \     __/
+        b \ __/  a
+           C
+
+        By convention, lowercase letter is used to mean the side length
+        opposite a vertex, and the uppercase letter is used to mean the
+        angle of that vertex.
+
+        The sin law relates a, A, b, and B:
+
+        sin*(A) sin(b) = sin*(B) sin(a)
+        * not the spatial trig function, always the regular trig function
+
+        This specific method takes a, A, B and computes b.
+        """
+        math = self.math
+        real = math.real
+        a = to_real(real, a)
+        A = to_real(real, A)
+        B = to_real(real, B)
+        return self.asin(self.sin(a) / math.sin(a) * math.sin(b))
+    def sine_law_angle(self, a, A, b):
+        """
+        A triangle looks like this:
+
+             c
+        A -------- B
+         \     __/
+        b \ __/  a
+           C
+
+        By convention, lowercase letter is used to mean the side length
+        opposite a vertex, and the uppercase letter is used to mean the
+        angle of that vertex.
+
+        The sin law relates a, A, b, and B:
+
+        sin*(A) sin(b) = sin*(B) sin(a)
+        * not the spatial trig function, always the regular trig function
+
+        This specific method takes a, A, b and computes B.
+        """
+        math = self.math
+        real = math.real
+        a = to_real(real, a)
+        A = to_real(real, A)
+        b = to_real(real, b)
+        return math.asin(math.sin(A) / self.sin(a) * self.sin(b))
 
 class space_point(object):
     """
@@ -261,3 +620,12 @@ class space_point(object):
         return self.home.magnitude_of(self)
     def __add__(self, other):
         return self.home.parallel_transport(self, other)
+    def __neg__(self):
+        home = self.home
+        x = self.x
+        return space_point(
+            home=home,
+            x=(x[0],)+tuple(map(operator.neg, x[1:]))
+            )
+    def __sub__(self, other):
+        return self + -other
